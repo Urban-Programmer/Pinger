bb8a28536d9e09c228f81bc69ba28d2ab4406244
Urban-Programmer <54013870+Urban-Programmer@users.noreply.github.com>
2020-10-03 07:30:17 -0400

    Add project files.

---
+++ DeadlinkChecker/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace DeadlinkChecker
+{
+    partial class LinkForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LinkForm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.LogBox = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblChgStatus = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(414, 46);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(369, -3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(45, 49);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBoxIP
+            // 
+            this.textBoxIP.Location = new System.Drawing.Point(90, 69);
+            this.textBoxIP.Name = "textBoxIP";
+            this.textBoxIP.Size = new System.Drawing.Size(211, 23);
+            this.textBoxIP.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Sitka Text", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label1.Location = new System.Drawing.Point(33, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ping:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(307, 69);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(85, 23);
+            this.btnSubmit.TabIndex = 3;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // LogBox
+            // 
+            this.LogBox.BackColor = System.Drawing.SystemColors.MenuText;
+            this.LogBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.LogBox.ForeColor = System.Drawing.SystemColors.Window;
+            this.LogBox.FormattingEnabled = true;
+            this.LogBox.ItemHeight = 15;
+            this.LogBox.Location = new System.Drawing.Point(12, 108);
+            this.LogBox.Name = "LogBox";
+            this.LogBox.Size = new System.Drawing.Size(380, 330);
+            this.LogBox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Sitka Banner", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(32, 441);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 35);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Status: ";
+            // 
+            // lblChgStatus
+            // 
+            this.lblChgStatus.AutoSize = true;
+            this.lblChgStatus.Font = new System.Drawing.Font("Sitka Banner", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblChgStatus.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.lblChgStatus.Location = new System.Drawing.Point(117, 441);
+            this.lblChgStatus.Name = "lblChgStatus";
+            this.lblChgStatus.Size = new System.Drawing.Size(139, 35);
+            this.lblChgStatus.TabIndex = 6;
+            this.lblChgStatus.Text = "Click Submit";
+            this.lblChgStatus.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // LinkForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
+            this.ClientSize = new System.Drawing.Size(414, 487);
+            this.Controls.Add(this.lblChgStatus);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LogBox);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxIP);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LinkForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DeadLink Checker";
+            this.Load += new System.EventHandler(this.LinkForm_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBoxIP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.ListBox LogBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblChgStatus;
+    }
+}
+
---
+++ DeadlinkChecker/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+
+namespace DeadlinkChecker
+{
+    public partial class LinkForm : Form
+    {
+        public LinkForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LinkForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {  
+
+            Ping ping = new Ping();
+            PingReply reply = ping.Send(textBoxIP.Text, 1000);
+            if (reply.Status == IPStatus.Success)
+            {
+                LogBox.Items.Add("Address: {0} " + reply.Address.ToString());
+                LogBox.Items.Add("RoundTrip time: {0} " + reply.RoundtripTime);
+                LogBox.Items.Add("Time to live: {0} " + reply.Options.Ttl);
+                LogBox.Items.Add("Don't fragment: {0} "+ reply.Options.DontFragment);
+                LogBox.Items.Add("Buffer size: {0} " + reply.Buffer.Length);
+
+
+
+
+            }
+
+            lblChgStatus.Text = reply.Status.ToString();
+
+
+
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
